d1f01c5dbc48b4b3fff9451fc87333614f21a8d3
Menti <josementi@gmail.com>
2016-11-09 00:48:22 +0000

    Start of fader and spawn zone

---
+++ Assets/Fader.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fader : MonoBehaviour {
+
+    public float SourceAlpha;
+    public float TargetAlpha;
+    public float Delta;
+    public Image RenderTarget;
+    private float currentAlpha;
+
+    public bool Loop;
+    public bool PlayOnAwake;
+    private bool isFading;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayOnAwake)
+        {
+            isFading = true;
+        }
+	}
+
+    void Update()
+    {
+        if (isFading)
+        {
+            currentAlpha = Mathf.Lerp(SourceAlpha, TargetAlpha, Delta);
+            if (Loop)
+            {
+                if (Mathf.Abs(SourceAlpha - TargetAlpha) <= 0.05f)
+                {
+                    var swap = SourceAlpha;
+                    SourceAlpha = TargetAlpha;
+                    TargetAlpha = swap;
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(SourceAlpha - TargetAlpha) <= 0.05f)
+                {
+                    currentAlpha = TargetAlpha; 
+                }
+            }
+            RenderTarget.color = new Color(RenderTarget.color.r, RenderTarget.color.g, RenderTarget.color.b, currentAlpha);
+        }
+    }
+
+    public void StartFade()
+    {
+        isFading = true;
+    }
+
+
+}
---
+++ Assets/SpriteGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteGenerator : MonoBehaviour {
+
+    public Texture2D Texture;
+    public PolygonCollider2D Collider;
+    private Sprite sprite;
+    public SpriteRenderer SRenderer;
+
+	// Use this for initialization
+	void Start () {
+        sprite = Sprite.Create(Texture, new Rect(10, 10, 15, 20), Vector2.zero, 1);
+        SRenderer.sprite = sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
